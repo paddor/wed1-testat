ab5abcc0360da71cbc21394acdc57ca0b5a83501
Patrik Wenger <paddor@gmail.com>
2016-05-24 02:55:37 +0200

    make font resizable using font slider

--- scripts/font-size.js
+++ scripts/font-size.js
@@ -1,1 +1,14 @@
+'use strict';
+window.addEventListener('load', function () {
+  var fontSlider = document.getElementById('font-slider');
+  document.getElementById('font-size-form').addEventListener('change', function () {
+    // get desired size
+    var fontSize = (fontSlider.valueOf().value)/10;
 
+    // change font size on root element
+    document.getElementsByTagName("html")[0].style.fontSize = fontSize + 'rem';
+
+    // show new value
+    document.getElementById('font-size').innerHTML = fontSize;
+  });
+});
